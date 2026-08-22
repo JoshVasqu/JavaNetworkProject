75336b6bca93caf19fbd6cbce945ee87ca4220f6
JoshVasqu <155032803+JoshVasqu@users.noreply.github.com>
2026-08-22 14:30:41 -0500

    HTTPServer OOP Update

--- HTTPServer.java
+++ HTTPServer.java
@@ -5,29 +5,57 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class HTTPServer {
-    private Socket clientSocket = null;
+    private HashMap<String, Socket> clients = new HashMap<>();
     private ServerSocket serverSocket = null;
     private String serverIp = null;
+    private int serverPort = 8080;
 
-    public HTTPServer(int port, String serverIp) {
-        // start server and wait for connection
+    public HTTPServer(int serverPort, String serverIp) {
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+    }
+
+    public void start() throws IOException {
         try {
-            serverSocket = new ServerSocket(port, 50, InetAddress.getByName(serverIp));
+            serverSocket = new ServerSocket(serverPort, 50, InetAddress.getByName(serverIp));
             System.out.println("Server started");
-            System.out.println("Waiting for a client ...");
-            System.out.println("http://" + serverIp + ":" + port);
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+    }
 
-            // connection
-            clientSocket = serverSocket.accept();
-            System.out.println("Client accepted");
+    public void run(boolean running) throws IOException{
+        if (serverSocket == null) {
+            start();
+        }
 
-            // send data to client
+        System.out.println("Waiting for a client ...");
+        System.out.println("http://" + serverIp + ":" + serverPort);
+        while(running) {
+            Socket client = connectClient();
+            sendDataToClient(client);
+            printDataFromClient(client);
+        }
+        disconnectAll();
+    }
+
+    public void sendDataToClient(Socket clientSocket) throws IOException{
+        try {
             Date today = new Date();
             String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
             clientSocket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+        } catch(IOException i) {
+            System.out.println(i);
+            return;
+        }
+    }
 
+    public void printDataFromClient(Socket clientSocket) {
+        try {
             InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
             BufferedReader reader = new BufferedReader(isr);
             String line = reader.readLine();
@@ -37,16 +65,40 @@ public class HTTPServer {
             }
             isr.close();
             reader.close();
-            
-            System.out.println("Closing connection");
-
-            clientSocket.close();
-        } catch(IOException i) {
+        } catch (IOException i) {
             System.out.println(i);
         }
     }
 
-    public static void main(String args[]) {
+    private Socket connectClient() throws IOException{
+        try {
+            Socket clientSocket = serverSocket.accept();
+            String clientId = UUID.randomUUID().toString();
+            clients.put(clientId, clientSocket);
+            return clientSocket;
+        } catch (IOException i) {
+            System.out.println(i);
+            return null;
+        }
+    }
+
+    private void disconnectClient(Socket clientSocket) throws IOException{
+        try {
+            clientSocket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    private void disconnectAll() throws IOException{
+        for (String key : clients.keySet()) {
+            disconnectClient(clients.get(key));
+        }
+    }
+
+    public static void main(String args[]) throws IOException{
         HTTPServer server = new HTTPServer(8080, "192.168.1.17");
+        server.run(true);
     }
 }
+
--- ServerLevel.java
+++ ServerLevel.java
@@ -1,4 +1,11 @@
 public enum ServerLevel {
     LOCAL(0),
-    
+    WIFI(1),
+    INTERNET(2);
+
+    private final int level;
+
+    ServerLevel(int level) {
+        this.level = level;
+    }
 }
